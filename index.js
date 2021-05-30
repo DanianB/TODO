561201560b7a1157edf832954f409cfe30a3750c
DanianB <78708804+DanianB@users.noreply.github.com>
2021-05-31 09:55:05 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 function App(){
-    const [todos, setTodos] = React.useState([
-        {
-            text: 'learn react',
-            isCompleted: false,
-        },
-        {
-            text: 'meet friend for lunch',
-            isCompleted: false,
-        },
-        {
-            text: 'build todo app',
-            isCompleted: false,
-        }
-    ]);
-    return(<>
-    {todos.map((todo,) => <div key={i}>{todo.text}</div>)}
-    </>);
+  const [todos, setTodos] = React.useState([
+    {
+      text: 'learn react',
+      isCompleted: false,
+    },
+    {
+      text: 'meet friend for lunch',
+      isCompleted: false,
+    },
+    {
+      text: 'build todo app',
+      isCompleted: false,
+    }        
+  ])
+
+  return(
+    <>
+      {todos.map((todo, i) => <div key={i}>{todo.text}</div>)}
+    </>
+  );
 }
 
-ReactDom.render(
-    <App/>,
-    document.getElementById('root')
-)
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
